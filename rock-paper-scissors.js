d28b87c4076962f884e9f15a023130bedb9b60d3
Tasha C <tashachang2@gmail.com>
2021-12-28 20:43:20 -0800

    Finished computerPlay and playerPlay

---
+++ rock-paper-scissors.js
@@ -0,0 +1,8 @@
+<script>
+  function computerPlay(){
+    const choices = ['rock', 'paper', 'scissors'];
+    let number = choices.length;
+    let randomNum = math.floor(math.random() * (number+1);
+    console.log(randomNum);
+  }
+</script>
