fa0715a22856e64e4443ee772eb090c380667ad4
Tasha C <tashachang2@gmail.com>
2021-12-28 22:42:12 -0800

    add prompt and toLowerCase

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,58 @@
-<script>
-  function computerPlay(){
-    const choices = ['rock', 'paper', 'scissors'];
-    let number = choices.length;
-    let randomNum = math.floor(math.random() * (number+1);
-    console.log(randomNum);
+const choices = ['rock', 'paper', 'scissors'];
+const number = choices.length;
+
+function computerPlay(){
+      const randomNum = Math.floor(Math.random() * number);
+  const computerChoice = choices[randomNum];
+  return computerChoice;
+}    
+let computerScore = 0;
+let playerScore = 0;
+
+function playRound(){
+  let computerSelection= computerPlay();
+  let playerSelection= prompt("What's your move?", "Rock, paper, or scissors").toLowerCase();
+   if ((computerSelection == "rock") && (playerSelection == "scissors")){
+    return ("Computer wins!");
   }
-</script>
+  else if ((computerSelection == "rock") && (playerSelection == "paper")){
+    return ("Player wins!");
+  }
+  else if ((computerSelection == "scissors") && (playerSelection == "rock")){
+    return ("Player wins!");
+  }
+  else if ((computerSelection == "scissors") && (playerSelection == "paper")){
+    return ("Computer wins!");
+  }
+  else if ((computerSelection == "paper") && (playerSelection == "scissors")){
+    return ("Player wins!");
+  }
+  else if ((computerSelection == "paper") && (playerSelection == "rock")){
+    return ("Computer wins!");
+  }
+  else {
+    return ("It's a tie!");
+  }
+}
+
+function game(){
+  for (i=0; i<=4; i++){
+    let result = playRound();
+    console.log(result);
+    if (result == ("Computer wins!")){
+      computerScore++;
+    }
+    else if (result == "Player wins!"){
+      playerScore++;
+    }
+  }
+  if (playerScore > computerScore){
+    console.log('You won!');
+  }
+  else if (computerScore > playerScore){
+    console.log('Sorry, you lost');
+  }
+  else{
+    console.log("Game is tied")
+  }
+}
